e060db5b713d245cd87b5d8863fe5ac81af816f9
Snbidkar <snehabidkar47@gmail.com>
2022-09-20 09:39:21 +0530

    Revert "Removed FullName Property"
    This reverts commit edb0098168466015fa7b1c46a02719adedac0adc.

--- MVCCrudWithDapper/Models/Employee.cs
+++ MVCCrudWithDapper/Models/Employee.cs
@@ -6,5 +6,7 @@
         public string? Name { get; set; }
         public int Age { get; set; }
         public string? LastName { get; set; }
+
+        public string? FullName { get; set; }
     }
 }
