b8fc66a0c207f80021a63ba860795d0bd66a2786
Snbidkar <snehabidkar47@gmail.com>
2022-09-20 09:40:55 +0530

    Revert "Commented code"
    This reverts commit 5b8b8f98335489109432304bed809045c5d33925.

--- MVCCrudWithDapper/Models/Employee.cs
+++ MVCCrudWithDapper/Models/Employee.cs
@@ -7,6 +7,6 @@
         public int Age { get; set; }
         public string? LastName { get; set; }
 
-      //public string? FullName { get; set; }
+        public string? FullName { get; set; }
     }
 }
